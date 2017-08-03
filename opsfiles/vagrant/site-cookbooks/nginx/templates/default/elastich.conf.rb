cea1ff9ab4dece51d534415d61659c358b91ff9a
Thien Tran <eva.thientran@gmail.com>
2017-08-03 12:03:12 +0700

    Update config nginx

--- opsfiles/vagrant/site-cookbooks/nginx/templates/default/elastich.conf.rb
+++ opsfiles/vagrant/site-cookbooks/nginx/templates/default/elastich.conf.rb
@@ -4,7 +4,7 @@ server {
     server_name <%= @domain %>;
 
     index index.php index.html index.htm;
-    set $root_path <%= @doc_root %>;
+    set $root_path <%= @doc_root %>/app;
     root $root_path;
 
     try_files $uri $uri/ @rewrite;
